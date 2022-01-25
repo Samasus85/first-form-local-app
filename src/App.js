2c62a24a5feb3bed795ebb20da200b428d794d08
Samasus85 <89699068+Samasus85@users.noreply.github.com>
2022-01-25 17:39:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-// import Container from './components/UI/Container/Container';
-// import Button from './components/UI/Buttons/Button';
 import Form from './components/Forms/Form';
 
 function App() {
